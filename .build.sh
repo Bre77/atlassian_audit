3fe02a61d175b844b2371e688010c71cb48226d7
Brett <brett.whynot@gmail.com>
2024-09-12 11:27:00 +1000

    Update build

--- .build.sh
+++ .build.sh
@@ -5,9 +5,5 @@ chmod -R u=rwX,go= *
 chmod -R u-x+X *
 chmod -R u=rwx,go= *
 python3.9 -m pip install --upgrade -t lib -r lib/requirements.txt --no-dependencies
-rm -rf lib/splunklib/__pycache__
-rm -rf lib/splunklib/searchcommands/__pycache__
-rm -rf lib/splunklib/modularinput/__pycache__
-rm -rf lib/*/__pycache__
 cd ..
 tar -cpzf $OUTPUT --exclude=.* --overwrite atlassian_audit 
